21240569484e3c5a71cb5e5c1f00933c06d2beea
Manuel Silva <msilva28.dev@gmail.com>
2017-03-13 08:21:55 -0700

    Initial commit;

---
+++ app/src/main/java/io/memetic/alcohdex/entries/AddEntryActivity.java
@@ -0,0 +1,15 @@
+package io.memetic.alcohdex.entries;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * TODO class description
+ *
+ * @author manuel.silva@mobileforming.com
+ *         3/7/17
+ */
+public class AddEntryActivity extends AppCompatActivity {
+
+
+
+}
