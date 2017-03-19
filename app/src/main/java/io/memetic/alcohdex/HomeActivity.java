b95b58bfc8ad3047e7786618e1acf17851f776f5
Manuel Silva <msilva28.dev@gmail.com>
2017-03-19 03:20:34 -0700

    Added new entry flow;

--- app/src/main/java/io/memetic/alcohdex/HomeActivity.java
+++ app/src/main/java/io/memetic/alcohdex/HomeActivity.java
@@ -1,9 +1,9 @@
 package io.memetic.alcohdex;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import io.memetic.alcohdex.entries.AddEntryActivity;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,9 +29,11 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action",
-                        Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(HomeActivity.this, AddEntryActivity.class);
+                startActivity(intent);
+                //                Snackbar.make(view, "Replace with your own action",
+                //                        Snackbar.LENGTH_LONG)
+                //                        .setAction("Action", null).show();
             }
         });
 
--- app/src/main/java/io/memetic/alcohdex/entries/AddEntryActivity.java
+++ app/src/main/java/io/memetic/alcohdex/entries/AddEntryActivity.java
@@ -1,6 +1,13 @@
 package io.memetic.alcohdex.entries;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+
+import io.memetic.alcohdex.R;
+import io.memetic.alcohdex.databinding.ActivityAddEntryBinding;
 
 /**
  * TODO class description
@@ -10,6 +17,26 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class AddEntryActivity extends AppCompatActivity {
 
+    public static final String TAG_ADD_ENTRY_FRAGMENT = "TAG_ADD_ENTRY_FRAGMENT";
+    private ActivityAddEntryBinding binding;
+    private AddEntryFragment fragment;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_entry);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            fragment = new AddEntryFragment();
+            transaction.add(binding.fragmentTarget.getId(), fragment, TAG_ADD_ENTRY_FRAGMENT);
+            transaction.commit();
+        }
+        else {
+            fragment = (AddEntryFragment) getSupportFragmentManager().findFragmentByTag(TAG_ADD_ENTRY_FRAGMENT);
+        }
+    }
 
 }
